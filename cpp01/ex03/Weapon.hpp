da2faa0af0127fa885480e56dd7a3d9a6276e9ec
Etienne Duplessix <edupless@c3r1s3.42prague.com>
2024-02-21 21:10:48 +0100

    v5

--- cpp01/ex03/Weapon.hpp
+++ cpp01/ex03/Weapon.hpp
@@ -1,5 +1,17 @@
 #ifndef WEAPON_HPP
 #define WEAPON_HPP
 
+#include <iostream>
 
-#endif /* WEAPON_HPP */
+class Weapon
+{
+    private:
+        std::string type;
+    public:
+        Weapon();
+        Weapon(std::string type);
+        const std::string   getType();
+        void    setType(std::string type);
+};
+
+#endif 
