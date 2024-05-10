1d3f7368edfb4a3b826b8142274411b452f0dcf4
Etienne Duplessix <edupless@c3r2s4.42prague.com>
2024-05-10 13:12:07 +0200

    v18888

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,55 +1,14 @@
 #include <iostream>
 #include "iter.hpp"
-#include <iostream>
-#include <Array.hpp>
+int main() {
+   
+    int intArray[] = {1, 2, 3, 4, 5};
+    iter(intArray, 5, printElement<int>); 
 
-#define MAX_VAL 750
-int main(int, char**)
-{
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+    std::cout << std::endl;
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
+    char charArray[] = {'a', 'b', 'c', 'd', 'e'};
+    iter(charArray, 5, printElement<char>); 
     return 0;
 }
